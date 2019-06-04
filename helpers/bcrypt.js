6a0b321b9be735006cbb76e68ebd502c3a8bb296
vermaneha17 <verma92.nv@gmail.com>
2019-06-04 12:53:33 +0530

    bcryptjs helper added

---
+++ helpers/bcrypt.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(parseInt(process.env.PASSWORD_SALT_ROUNDS));
+
+module.exports = {
+    hash: password => {
+        if (!password || String(password).trim() === '')
+            throw new Error('Password cannot be null.');
+        return bcrypt.hash(password, salt);
+    },
+    compare: (password, hash) => {
+        return bcrypt.compare(password, hash);
+    }
+};
